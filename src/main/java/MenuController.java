cdcc6f2fc5fc27b711732bf445df9d5c12b83ce3
calvinSteinbachSchule <steinbachcalvin@student.dv-schulen.de>
2026-01-16 08:48:52 +0100

    Feature: MenuScreen implementiert

---
+++ src/main/java/MenuController.java
@@ -0,0 +1,58 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController
+{
+    private Stage stage;
+    private MediaPlayer mediaPlayer;
+    private AudioClip clickSound;
+
+    @FXML
+    public void startButtonClicked()
+    {
+        mediaPlayer.stop();
+
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GameScreen.fxml"));
+            Parent rootGameScreen = loader.load();
+            stage.setScene(new Scene(rootGameScreen));
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void buttonMouseEntered()
+    {
+        clickSound.play();
+    }
+
+    @FXML
+    public void initialize()
+    {
+        clickSound = new AudioClip(getClass().getResource("/buttonClick.mp3").toString());
+        clickSound.setVolume(1.0);
+
+    }
+
+    public void setStage(Stage stage)
+    {
+        this.stage = stage;
+    }
+
+    public void setMediaPlayer(MediaPlayer mediaPlayer)
+    {
+        this.mediaPlayer = mediaPlayer;
+    }
+}
