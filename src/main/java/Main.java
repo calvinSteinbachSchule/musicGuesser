976fea892763950d2cc5e546fdb464889bbc873f
calvinSteinbachSchule <steinbachcalvin@student.dv-schulen.de>
2026-01-18 19:34:30 +0100

    Update: HighscoreService in Main Implementiert

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.Parent;
+import service.HighscoreService;
 
 public class Main extends Application
 {
+    private HighscoreService highscoreService;
+
     @Override
     public void start(Stage stage) throws Exception
     {
+        highscoreService = HighscoreService.getInstance();
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/StartScreen.fxml"));
         Parent rootStartScreen = loader.load();
         StartController startController = loader.getController();
@@ -21,6 +26,23 @@ public class Main extends Application
         stage.setTitle("MusicGuesser");
         stage.setScene(new Scene(rootStartScreen));
         stage.show();
+
+        stage.setOnCloseRequest(event -> {
+            if(highscoreService != null)
+            {
+                highscoreService.speichereHighscores(); // Speichert beim Schließen
+            }
+        });
+    }
+
+    @Override
+    public void stop() throws Exception
+    {
+        if(highscoreService != null)
+        {
+            highscoreService.speichereHighscores(); // Speichert beim Beenden
+        }
+        super.stop();
     }
 
     public static void main(String[] args)
